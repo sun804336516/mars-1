4de4150f01aaf9ecdd21af1e7f46356249ba8f6e
孙贤武mbp(m1) <sunxianwu@pyou.com>
2024-07-23 13:12:14 +0800

    mars

---
+++ mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "209695fc"
+#define MARS_PATH "HEAD"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2024-07-19 01:23:32"
+#define MARS_TAG ""
+
+#endif
